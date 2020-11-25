07ac118022006f5f97323802dcbad5ca35fc4c33
xianshiliang <1812099208@qq.com>
2020-11-25 21:43:04 +0800

    我的第一次提交

---
+++ src/main/java/com/spring/Person.java
@@ -0,0 +1,45 @@
+package com.spring;/*
+ *@author XSL
+ *@date 2020/11/25-17:38
+ *@Description:com.spring
+ */
+
+public class Person {
+    String name;
+    String age;
+
+    public Person() {
+    }
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public void eat(){
+        System.out.println("我爱吃");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/spring/TEST.java
@@ -0,0 +1,28 @@
+package com.spring;/*
+ *@author XSL
+ *@date 2020/11/25-17:13
+ *@Description:com.spring
+ */
+
+import java.io.Serializable;
+
+public class TEST implements Serializable {
+    private static final int AGE = 10;
+//    public static final int NAME=20
+
+
+    private static final long serialVersionUID = -7860404952359250319L;
+
+    public static void main(String[] args) {
+        String abc = "a";
+        System.out.println(abc);
+        Person person = new Person();
+        person.setAge("10");
+        person.setName("xcy");
+
+        person.eat();
+        System.out.println(person.toString());
+
+
+    }
+}
